743f04d2fc22a358f26994dc8fe669a403a3baf6
Unknown <bence.soregi0@gmail.com>
2018-04-23 13:26:05 +0200

    Forráskód

--- atva/js/script.js
+++ atva/js/script.js
@@ -75,7 +75,8 @@ $(document).ready(function() {
     var get_longitude = $('#google-map').data('longitude');
 
     function initialize_google_map() {
-        var myLatlng = new google.maps.LatLng(get_latitude, get_longitude);
+	
+		var myLatlng = new google.maps.LatLng(get_latitude, get_longitude);
         var mapOptions = {
             zoom: 14,
             scrollwheel: false,
@@ -84,9 +85,13 @@ $(document).ready(function() {
         var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
         var marker = new google.maps.Marker({
             position: myLatlng,
+			label: 'ATVA Consulting Kft.',
+			animation: google.maps.Animation.BOUNCE,
             map: map
         });
     }
     google.maps.event.addDomListener(window, 'load', initialize_google_map);
 
+	
+	
 });
